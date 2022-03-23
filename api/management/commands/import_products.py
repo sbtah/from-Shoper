674df7054e47377c0827470d0e5ebfa7cc528767
Sbtah <shisastore@protonmail.com>
2022-03-23 07:01:09 +0100

    Done some changes on Product model. Manage.py command for importing products is working.

--- api/management/commands/import_products.py
+++ api/management/commands/import_products.py
@@ -3,8 +3,7 @@ import time
 import os
 from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
-
-# from products.models import Product
+from products.models import Product
 
 
 # Get private data fron .env variable
@@ -54,25 +53,123 @@ def copy_all_products_from_shoper_api():
         items = res.get("list")
 
         for i in items:
-            print(i)
-            print(
-                "==============================================================================="
-            )
-            title = i.get("translations").get("en_GB").get("name")
-            description = i.get("translations").get("en_GB").get("description")
-            vendor = "MeowBaby"
+            try:
+                shoper_title_pl = i.get("translations").get("pl_PL").get("name")
+            except AttributeError:
+                shoper_title_pl = ""
+
+            try:
+                shoper_title_en = i.get("translations").get("en_GB").get("name")
+            except AttributeError:
+                shoper_title_en = ""
+
+            try:
+                shoper_title_de = i.get("translations").get("de_DE").get("name")
+            except AttributeError:
+                shoper_title_de = ""
+
+            try:
+                shoper_title_fr = i.get("translations").get("fr_FR").get("name")
+            except AttributeError:
+                shoper_title_fr = ""
+
+            try:
+                shoper_description_pl = (
+                    i.get("translations").get("pl_PL").get("description")
+                )
+            except AttributeError:
+                shoper_description_pl = ""
+
+            try:
+                shoper_description_en = (
+                    i.get("translations").get("en_GB").get("description")
+                )
+            except AttributeError:
+                shoper_description_en = ""
+
+            try:
+                shoper_description_fr = (
+                    i.get("translations").get("de_DE").get("description")
+                )
+            except AttributeError:
+                shoper_description_fr = ""
+
+            try:
+                shoper_description_de = (
+                    i.get("translations").get("fr_FR").get("description")
+                )
+            except AttributeError:
+                shoper_description_de = ""
+
+            vendor_brand = SHOPER_STORE[0:-3].capitalize()
             shoper_id = i.get("product_id")
-            sku = i.get("code")
-            ean = i.get("ean")
-            shoper_weight = i.get("vol_weight")
-            # print(title, description, vendor, shoper_id, sku, ean)
-            # print(
-            #     "==============================================================================="
-            # )
-            # Product.objects.create()
+
+            try:
+                shoper_ean = i.get("ean")
+            except AttributeError:
+                shoper_ean = ""
+
+            shoper_sku = i.get("code")
+
+            try:
+                shoper_weight = i.get("vol_weight")
+            except AttributeError:
+                shoper_weight = ""
+            is_active_shoper = i.get("stock").get("active")  # dunno if this is right
+            created_shoper = i.get("add_date")
+
+            try:
+                updated_shoper = i.get("edit_date")
+            except AttributeError:
+                updated_shoper = ""
+
+            shoper_price = i.get("stock").get("price")
+            shoper_gauge_id = i.get("gauge_id")
+
+            Product.objects.create(
+                shoper_title_pl=shoper_title_pl,
+                shoper_title_en=shoper_title_en,
+                shoper_title_de=shoper_title_de,
+                shoper_title_fr=shoper_title_fr,
+                shoper_description_pl=shoper_description_pl,
+                shoper_description_en=shoper_description_en,
+                shoper_description_fr=shoper_description_fr,
+                shoper_description_de=shoper_description_de,
+                vendor_brand=vendor_brand,
+                shoper_id=shoper_id,
+                shoper_ean=shoper_ean,
+                shoper_sku=shoper_sku,
+                shoper_weight=shoper_weight,
+                is_active_shoper=is_active_shoper,
+                created_shoper=created_shoper,
+                updated_shoper=updated_shoper,
+                shoper_price=shoper_price,
+                shoper_gauge_id=shoper_gauge_id,
+            )
             time.sleep(1)
+            print(f"Product created: {shoper_id} ")
 
     return
 
 
-copy_all_products_from_shoper_api()
+# copy_all_products_from_shoper_api()
+
+
+def clear_data():
+    Product.objects.all().delete()
+
+
+class Command(BaseCommand):
+    """Main class for command object that imports images from shoper."""
+
+    def handle(self, *args, **options):
+        """Custom handle method."""
+
+        clear_data()
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Database cleared Product objects count: {Product.objects.all().count()}"
+            )
+        )
+        copy_all_products_from_shoper_api()
+        self.stdout.write(self.style.SUCCESS("Database available!"))
--- products/models.py
+++ products/models.py
@@ -5,40 +5,44 @@ from images.models import Image
 class Product(models.Model):
     """Model for Product object."""
 
-    shoper_title_pl = models.CharField(max_length=200)
-    shoper_title_en = models.CharField(max_length=200, blank=True)
-    shoper_title_fr = models.CharField(max_length=200, blank=True)
-    shoper_title_de = models.CharField(max_length=200, blank=True)
-    shoper_description_pl = models.TextField(blank=True)
-    shoper_description_en = models.TextField(blank=True)
-    shoper_description_fr = models.TextField(blank=True)
-    shoper_description_de = models.TextField(blank=True)
+    shoper_title_pl = models.CharField(max_length=200)  #
+    shoper_title_en = models.CharField(max_length=200, blank=True)  #
+    shoper_title_fr = models.CharField(max_length=200, blank=True)  #
+    shoper_title_de = models.CharField(max_length=200, blank=True)  #
+    shoper_description_pl = models.TextField(blank=True)  #
+    shoper_description_en = models.TextField(blank=True)  #
+    shoper_description_fr = models.TextField(blank=True)  #
+    shoper_description_de = models.TextField(blank=True)  #
 
     shopify_title = models.CharField(max_length=200, blank=True, null=True)
     shopify_description = models.TextField(blank=True, null=True)
-    vendor_brand = models.CharField(max_length=100)
+
+    vendor_brand = models.CharField(max_length=100, blank=True)  #
     shoper_id = models.IntegerField(unique=True)
     shopify_id = models.IntegerField(unique=True, blank=True, null=True)
-    shoper_ean = models.CharField(max_length=13, unique=True, blank=True)
-    shoper_sku = models.CharField(max_length=25, unique=True, blank=True)
-    shopify_ean = models.CharField(max_length=13, unique=True, blank=True)
-    shopify_sku = models.CharField(max_length=25, unique=True, blank=True)
+    shoper_ean = models.CharField(max_length=13, blank=True)  #
+    shoper_sku = models.CharField(max_length=25, unique=True, blank=True)  #
+    shopify_ean = models.CharField(max_length=13, blank=True)
+    shopify_sku = models.CharField(max_length=25, blank=True)
     images = models.ManyToManyField(Image, blank=True)
-    shopify_position = models.IntegerField(null=True)
-    shoper_weight = models.IntegerField(null=True)
-    is_active_shoper = models.BooleanField(default=False)
+    shopify_position = models.IntegerField(blank=True, null=True)
+    shoper_weight = models.CharField(max_length=100, blank=True)  #
+
+    is_active_shoper = models.BooleanField(default=False)  #
     is_active_shopify = models.BooleanField(default=False)
-    shopify_tags = models.CharField(max_length=200, blank=True, null=True)
+    shoper_tags = models.CharField(max_length=200, blank=True)
+    shopify_tags = models.CharField(max_length=200, blank=True)
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    created_shoper = models.DateTimeField()
-    created_shopify = models.DateTimeField(null=True)
+    created_shoper = models.DateTimeField()  # add_date
+    updated_shoper = models.DateTimeField(null=True)  # edit_date
     created_shopify = models.DateTimeField(null=True)
     updated_shopify = models.DateTimeField(null=True)
-    shoper_price = models.CharField(max_length=40)
-    shoper_gauge_id = models.IntegerField(null=True)
+    shoper_price = models.CharField(max_length=40)  # price
+    shoper_gauge_id = models.IntegerField(null=True)  # gauge_id
     shopify_seo_title = models.CharField(max_length=200, blank=True)
     shopify_seo_description = models.TextField(blank=True)
 
     def __str__(self):
-        return f"Title-{self.title};Shoper_ID-{self.shoper_id};Shopify_ID-{self.shopify_id}"
+        return f"Title-{self.shoper_title_pl};Shoper_ID-{self.shoper_id};Shopify_ID-{self.shopify_id}"

e8a4c04f91773e9140efff4a8a4549823a92dabf
Sbtah <shisastore@protonmail.com>
2022-03-25 07:02:52 +0100

    Added kill_db.py script in root directory of the project. Main function of the script is to find and delete migrations and test database files.

---
+++ kill_db.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+
+
+apps = [
+    "api",
+    "products",
+    "images",
+]
+
+
+def remove_db():
+    """Remove db files from sqlite test db."""
+
+    for file in os.listdir():
+        if "db.sqlite3" in file:
+            os.remove(file)
+            print(f"{file} removed!")
+
+
+def remove_migrations():
+    """Remove migration files in migrations directory."""
+    for x in os.listdir():
+        if x != "__init__.py":
+            try:
+                shutil.rmtree(x)
+            except NotADirectoryError as e:
+                os.remove(x)
+            print(f"File: {x} removed!")
+
+
+def find_migrations():
+    """Find migration directories in specified apps."""
+
+    for directory in os.listdir():
+        if directory in apps:
+            os.chdir(directory)
+            for file in os.listdir():
+                if file == "migrations":
+                    os.chdir(file)
+                    remove_migrations()
+                    os.chdir("..")
+            os.chdir("..")
+
+
+remove_db()
+find_migrations()

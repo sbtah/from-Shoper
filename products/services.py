98f2cc064f622b3bafb9041fda6276dee0e24234
sbtah <shisastore@protonmail.com>
2022-05-19 08:18:35 +0100

    Added serivces.py for calling Shoper API from view. Added ProductUpdateFromShoperView in products views.py that calls Shoper API and updates product instance if date of update on Shoper is greater then date of update in DB

--- products/services.py
+++ products/services.py
@@ -1,7 +1,5 @@
 import requests
-import time
 import os
-import json
 from dotenv import load_dotenv
 
 
@@ -34,4 +32,120 @@ def get_single_product(id):
     response = requests.get(url, headers=headers)
     product = response.json()
 
-    return product
+    try:
+        shoper_title_pl = product.get("translations").get("pl_PL").get("name")
+    except AttributeError:
+        shoper_title_pl = ""
+
+    try:
+        shoper_title_en = product.get("translations").get("en_GB").get("name")
+    except AttributeError:
+        shoper_title_en = ""
+
+    try:
+        shoper_title_de = product.get("translations").get("de_DE").get("name")
+    except AttributeError:
+        shoper_title_de = ""
+
+    try:
+        shoper_title_fr = product.get("translations").get("fr_FR").get("name")
+    except AttributeError:
+        shoper_title_fr = ""
+
+    try:
+        shoper_description_pl = (
+            product.get("translations").get("pl_PL").get("description")
+        )
+    except AttributeError:
+        shoper_description_pl = ""
+
+    try:
+        shoper_description_en = (
+            product.get("translations").get("en_GB").get("description")
+        )
+    except AttributeError:
+        shoper_description_en = ""
+
+    try:
+        shoper_description_fr = (
+            product.get("translations").get("fr_FR").get("description")
+        )
+    except AttributeError:
+        shoper_description_fr = ""
+
+    try:
+        shoper_description_de = (
+            product.get("translations").get("de_DE").get("description")
+        )
+    except AttributeError:
+        shoper_description_de = ""
+
+    vendor_brand = SHOPER_STORE[0:-3].capitalize()
+    shoper_id = product.get("product_id")
+
+    try:
+        shoper_ean = product.get("ean")
+    except AttributeError:
+        shoper_ean = ""
+
+    try:
+        shoper_sku = product.get("code")
+    except AttributeError:
+        shoper_sku = ""
+
+    try:
+        shoper_weight = product.get("vol_weight")
+    except AttributeError:
+        shoper_weight = ""
+
+    try:
+        is_active_shoper = product.get("stock").get("active")
+    except AttributeError:
+        is_active_shoper = ""
+
+    try:
+        created_shoper = product.get("add_date")
+    except AttributeError:
+        created_shoper = ""
+
+    try:
+        updated_shoper = product.get("edit_date")
+    except AttributeError:
+        updated_shoper = ""
+
+    try:
+        is_on_shoper = f'{True if product.get("add_date") else ""}'
+    except AttributeError:
+        is_on_shoper = ""
+
+    try:
+        shoper_price = product.get("stock").get("price")
+    except AttributeError:
+        shoper_price = ""
+
+    try:
+        shoper_gauge_id = product.get("gauge_id")
+    except:
+        shoper_gauge_id = ""
+
+    return {
+        "shoper_title_pl": shoper_title_pl,
+        "shoper_title_en": shoper_title_en,
+        "shoper_title_de": shoper_title_de,
+        "shoper_title_fr": shoper_title_fr,
+        "shoper_description_pl": shoper_description_pl,
+        "shoper_description_en": shoper_description_en,
+        "shoper_description_fr": shoper_description_fr,
+        "shoper_description_de": shoper_description_de,
+        "vendor_brand": vendor_brand,
+        "shoper_id": shoper_id,
+        "shoper_ean": shoper_ean,
+        "shoper_sku": shoper_sku,
+        "shoper_weight": shoper_weight,
+        "is_active_shoper": is_active_shoper,
+        "created_shoper": created_shoper,
+        "updated_shoper": updated_shoper,
+        "is_on_shoper": is_on_shoper,
+        "shoper_price": shoper_price,
+        "shoper_gauge_id": shoper_gauge_id,
+    }
--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from products.models import Product
+from django.contrib import messages
 from django.urls import reverse, reverse_lazy
-from products.forms import ProductUpdateFromShoperForm
 from products.services import get_single_product
 
 
@@ -30,10 +31,50 @@ class ProductUpdateView(LoginRequiredMixin, generic.UpdateView):
     pass
 
 
-class ProductUpdateFromShoperView(LoginRequiredMixin, generic.UpdateView):
-    """UpdateView for Product object that pulls data from Shoper API."""
+class ProductUpdateFromShoperView(LoginRequiredMixin, generic.DetailView):
+    """Update data for Product from API."""
 
     model = Product
-    form_class = ProductUpdateFromShoperForm
-    template_name = "products/product_update_from_shoper.html"
-    success_url = reverse_lazy("panel:panel")
+    template_name = "products/product_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        product = self.get_object()
+        response = get_single_product(product.shoper_id)
+
+        if (
+            datetime.strptime(response["updated_shoper"], "%Y-%m-%d %H:%M:%S")
+            > product.updated_shoper
+        ):
+            product.shoper_title_pl = response["shoper_title_pl"]
+            product.shoper_title_en = response["shoper_title_en"]
+            product.shoper_title_de = response["shoper_title_de"]
+            product.shoper_title_fr = response["shoper_title_fr"]
+            product.shoper_description_pl = response["shoper_description_pl"]
+            product.shoper_description_en = response["shoper_description_en"]
+            product.shoper_description_fr = response["shoper_description_fr"]
+            product.shoper_description_de = response["shoper_description_de"]
+            product.vendor_brand = response["vendor_brand"]
+            product.shoper_id = response["shoper_id"]
+            product.shoper_ean = response["shoper_ean"]
+            product.shoper_sku = response["shoper_sku"]
+            product.shoper_weight = response["shoper_weight"]
+            product.is_active_shoper = response["is_active_shoper"]
+            product.created_shoper = response["created_shoper"]
+            product.updated_shoper = response["updated_shoper"]
+            product.shoper_price = response["shoper_price"]
+            product.shoper_gauge_id = response["shoper_gauge_id"]
+            product.is_on_shoper = response["is_on_shoper"]
+            product.save()
+            messages.success(request, ("Product Updated."))
+        else:
+            messages.success(request, ("There was nothing to update."))
+        return super().get(request, *args, **kwargs)
+
+
+def update_product_from_shoper(request, pk):
+
+    if request.method == "GET":
+        object = get_single_product(pk)
+        print(object)
+
+        return render(request, "products/product_detail.html")

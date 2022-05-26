a8825e8f56079736e7e364116dee73632f9e4cb5
sbtah <shisastore@protonmail.com>
2022-05-26 08:45:22 +0100

    Refactored PickLanguageToCopyForm to allow of picking a values of from_language and to_language - needed for creation of copy of Product object.

--- products/forms.py
+++ products/forms.py
@@ -1,32 +1,30 @@
-from pyexpat import model
 from django import forms
 from products.models import Product
-from products.services import get_single_product
 
 
-class ProductUpdateFromShoperForm(forms.ModelForm):
-    """Form for updating Product object from Shoper API."""
+# class ProductUpdateFromShoperForm(forms.ModelForm):
+#     """Form for updating Product object from Shoper API."""
 
-    class Meta:
-        model = Product
-        fields = [
-            "shoper_title_pl",
-            "shoper_title_en",
-            "shoper_title_fr",
-            "shoper_title_de",
-            "shoper_description_pl",
-            "shoper_description_en",
-            "shoper_description_de",
-            "vendor_brand",
-            "shoper_ean",
-            "shoper_sku",
-            "shoper_images",
-            "shoper_weight",
-            "is_active_shoper",
-            "shoper_tags",
-            "shoper_price",
-            "shoper_gauge_id",
-        ]
+#     class Meta:
+#         model = Product
+#         fields = [
+#             "shoper_title_pl",
+#             "shoper_title_en",
+#             "shoper_title_fr",
+#             "shoper_title_de",
+#             "shoper_description_pl",
+#             "shoper_description_en",
+#             "shoper_description_de",
+#             "vendor_brand",
+#             "shoper_ean",
+#             "shoper_sku",
+#             "shoper_images",
+#             "shoper_weight",
+#             "is_active_shoper",
+#             "shoper_tags",
+#             "shoper_price",
+#             "shoper_gauge_id",
+#         ]
 
 
 class PickLanguageToCopyForm(forms.Form):
@@ -35,9 +33,10 @@ class PickLanguageToCopyForm(forms.Form):
         widget=forms.Select(),
         choices=(
             [
+                ("", "Pick language"),
                 ("pl_PL", "PL"),
                 ("de_DE", "DE"),
-                ("en_EU", "EU"),
+                ("en_IE", "EU"),
                 ("en_GB", "GB"),
                 ("fr_FR", "FR"),
                 ("en_US", "US"),
@@ -50,9 +49,10 @@ class PickLanguageToCopyForm(forms.Form):
         widget=forms.Select(),
         choices=(
             [
+                ("", "Pick language"),
                 ("pl_PL", "PL"),
                 ("de_DE", "DE"),
-                ("en_EU", "EU"),
+                ("en_IE", "EU"),
                 ("en_GB", "GB"),
                 ("fr_FR", "FR"),
                 ("en_US", "US"),

6bf44fdd2e7839fcf9074d23c6ae6cd56c0b7e7e
sbtah <shisastore@protonmail.com>
2022-05-27 14:04:51 +0200

    Refactored import in import_produc management command

--- api/management/commands/import_products.py
+++ api/management/commands/import_products.py
@@ -1,30 +1,12 @@
 import requests
 import time
-import os
 from datetime import datetime
-from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from products.models import Product
+from external.token import get_token
+from external.token import SHOPER_STORE
 
 
-# Get private data fron .env variable
-load_dotenv()
-SHOPER_LOGIN = os.environ.get("SHOPER_LOGIN")
-SHOPER_PASSWORD = os.environ.get("SHOPER_PASSWORD")
-SHOPER_STORE = os.environ.get("SHOPER_STORE")
-
-
-# Get a token by login to your store.
-def get_token():
-    """Generates fresh token from SHOPER API."""
-
-    url_login = f"https://{SHOPER_STORE}/webapi/rest/auth"
-    response = requests.post(url_login, auth=(SHOPER_LOGIN, SHOPER_PASSWORD))
-    access_token = response.json().get("access_token")
-    return access_token
-
-
-# Get instance of TOKEN.
 TOKEN = get_token()
 
 

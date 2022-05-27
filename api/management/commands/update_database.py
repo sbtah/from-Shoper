cd1a724158855a6aa1d847738c8764eb0d31f7c7
sbtah <shisastore@protonmail.com>
2022-05-27 14:05:42 +0200

    Refactored import in update_database management command

--- api/management/commands/update_database.py
+++ api/management/commands/update_database.py
@@ -1,8 +1,3 @@
-import requests
-import time
-import os
-from datetime import datetime
-from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from images.models import Image
 from products.models import Product
@@ -10,27 +5,6 @@ from api.management.commands.import_products import copy_all_products_from_shope
 from api.management.commands.import_images import (
     copy_all_product_images_from_shoper_api,
 )
-
-
-# Get private data fron .env variable
-load_dotenv()
-SHOPER_LOGIN = os.environ.get("SHOPER_LOGIN")
-SHOPER_PASSWORD = os.environ.get("SHOPER_PASSWORD")
-SHOPER_STORE = os.environ.get("SHOPER_STORE")
-
-
-# Get a token by login to your store.
-def get_token():
-    """Generates fresh token from SHOPER API."""
-
-    url_login = f"https://{SHOPER_STORE}/webapi/rest/auth"
-    response = requests.post(url_login, auth=(SHOPER_LOGIN, SHOPER_PASSWORD))
-    access_token = response.json().get("access_token")
-    return access_token
-
-
-# Get instance of TOKEN.
-TOKEN = get_token()
 
 
 def clear_database():

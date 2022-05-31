bc09e462708c0006b7835c0863a39d4309e8ade1
sbtah <shisastore@protonmail.com>
2022-05-31 23:16:41 +0200

    Added csv_from_db management command that creates a needed csv rapport from current state of local db.

--- api/management/commands/csv_from_db.py
+++ api/management/commands/csv_from_db.py
@@ -1,60 +1,72 @@
-import pandas as pd
 from django.core.management.base import BaseCommand
 from products.models import Product
-from external.get_products import (
-    get_list_of_all_shoper_product_sku,
-    get_list_of_all_shoper_product_ids,
-)
 
 
-# CSV PATH TO CODES FOR LANGUAGE TO Update.
-CSV_PATH = "Images.csv"
-df = pd.read_csv(CSV_PATH, sep=";")
+def generate_simple_product_for_language(language, file_name):
 
+    with open(f"{file_name}.csv", "w") as file:
+        file.write(f"product_code;name")
+    # filter(field__like='10%8%0%')
+    query_set = Product.objects.filter(shoper_sku__endswith=f"{language[3:]}")
+    print(f"Number of products: {query_set.count()}")
 
-# def generate_product_data(lang_code, file_name, to_csv):
-
-#     with open(f"{file_name}.csv", "r") as file:
-#         file.write(f"Shoper ID;SKU;NAME;LINK;SEO-LINK")
-
-#     for sku in (f"{row.product_code}" for row in df.itertuples()):
-#         try:
-#             product = Product.objects.get(shoper_sku=sku)
-#             with open(f"{file_name}.csv", "a") as file:
-#                 file.write(
-#                     f"\n{product.shoper_id};{product.shoper_sku};{product.current_link};{product.seo_link}"
-#                 )
-
-#         except Product.DoesNotExist:
-#             continue
-
-
-def start():
-    return get_list_of_all_shoper_product_ids()
+    for item in query_set:
+        if language == "pl_PL":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_pl}")
+        elif language == "en_GB":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_gb})")
+        elif language == "en_IE":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_eu})")
+        elif language == "de_DE":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_de})")
+        elif language == "fr_FR":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_fr})")
+        elif language == "en_US":
+            print(item.shoper_id)
+            with open(f"{file_name}.csv", "a") as file:
+                file.write(f"\n{item.shoper_sku};{item.shoper_title_us})")
 
 
 class Command(BaseCommand):
     """Main class for command object that imports products from shoper."""
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument(
-    #         "on_language_tag",
-    #         type=str,
-    #         help="Indicates the translation that turned of on products.",
-    #     )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "language",
+            type=str,
+            help="Indicates the language TAG for product range to generate rapport.",
+        )
+        parser.add_argument(
+            "file_name",
+            type=str,
+            help="Indicates the language TAG for product range to generate rapport.",
+        )
 
     def handle(self, *args, **kwargs):
         """Custom handle method."""
 
+        lang = kwargs["language"]
+        file = kwargs["file_name"]
+
         self.stdout.write(
             self.style.SUCCESS(
                 f"""
-                Generate categories data.
+                Generate rapport data.
                 """
             )
         )
+        generate_simple_product_for_language(lang, file)
 
-        print(start())
         self.stdout.write(
             self.style.SUCCESS(
                 f"""

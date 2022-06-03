a159e9d0995927a851faa681b126f8dada393076
sbtah <shisastore@protonmail.com>
2022-06-03 21:16:28 +0100

    Modified fetch_images to first create all translations available for current Image object.

--- api/management/commands/fetch_images.py
+++ api/management/commands/fetch_images.py
@@ -1,21 +1,10 @@
 import requests
 import time
-from external.get_token import SHOPER_STORE, TOKEN
-from django.core.management.base import BaseCommand
 from images.models import Image
-
-
-# Get number of pages from image list API.
-def get_number_of_image_pages():
-    """Get number of all image pages from SHOPER api"""
-
-    url = f"https://{SHOPER_STORE}/webapi/rest/product-images"
-    headers = {"Authorization": f"Bearer {TOKEN}"}
-    response = requests.get(url, headers=headers)
-    res = response.json()
-    pages = res.get("pages")
-
-    return pages
+from translations.models import ImageTranslation
+from django.core.management.base import BaseCommand
+from external.get_token import SHOPER_STORE, TOKEN
+from external.get_images import get_number_of_image_pages
 
 
 def copy_all_product_images_from_shoper_api():
@@ -30,11 +19,27 @@ def copy_all_product_images_from_shoper_api():
         response = requests.get(url, headers=headers, params=data)
         res = response.json()
         items = res.get("list")
-        # tags = (tag for tag in res.get("translations"))
 
         for i in items:
-
             shoper_gfx_id = i.get("gfx_id")
+            print(shoper_gfx_id)
+            try:
+                for tag in i.get("translations"):
+                    print(tag)
+
+                    translation = ImageTranslation.objects.update_or_create(
+                        locale=tag,
+                        shoper_translation_id=i.get("translations")
+                        .get(tag)
+                        .get("translation_id"),
+                        related_gfx_id=i.get("translations").get(tag).get("gfx_id"),
+                        name=i.get("translations").get(tag).get("name"),
+                        lang_id=i.get("translations").get(tag).get("lang_id"),
+                    )
+                    print(f"Translation:{translation[0]}")
+                    print(f"Created:{translation[1]}")
+            except TypeError:
+                continue
 
             try:
                 shoper_product_id = i.get("product_id")
@@ -47,49 +52,14 @@ def copy_all_product_images_from_shoper_api():
                 shoper_main = ""
 
             try:
-                shoper_title_pl = i.get("translations").get("pl_PL").get("name")
-            except AttributeError:
-                shoper_title_pl = ""
-
-            try:
-                shoper_title_en = i.get("translations").get("en_GB").get("name")
-            except AttributeError:
-                shoper_title_en = ""
-
-            try:
-                shoper_title_de = i.get("translations").get("de_DE").get("name")
-            except AttributeError:
-                shoper_title_de = ""
-
-            try:
-                shoper_title_fr = i.get("translations").get("fr_FR").get("name")
-            except AttributeError:
-                shoper_title_fr = ""
-
-            try:
                 shoper_order = i.get("order")
             except AttributeError:
                 shoper_order = ""
 
             try:
-                shoper_link_pl = f'https://{SHOPER_STORE}/userdata/public/gfx/{i.get("gfx_id")}/{shoper_title_pl[:-4] if ".jpg" in shoper_title_pl else shoper_title_pl}.jpg'
+                shoper_image_name = i.get("name")
             except AttributeError:
-                shoper_link_pl = ""
-
-            try:
-                shoper_link_en = f'https://{SHOPER_STORE}/userdata/public/gfx/{i.get("gfx_id")}/{shoper_title_en[:-4] if ".jpg" in shoper_title_en else shoper_title_en}.jpg'
-            except AttributeError:
-                shoper_link_en = ""
-
-            try:
-                shoper_link_de = f'https://{SHOPER_STORE}/userdata/public/gfx/{i.get("gfx_id")}/{shoper_title_de[:-4] if ".jpg" in shoper_title_de else shoper_title_de}.jpg'
-            except AttributeError:
-                shoper_link_de = ""
-
-            try:
-                shoper_link_fr = f'https://{SHOPER_STORE}/userdata/public/gfx/{i.get("gfx_id")}/{shoper_title_fr[:-4] if ".jpg" in shoper_title_fr else shoper_title_fr}.jpg'
-            except AttributeError:
-                shoper_link_fr = ""
+                shoper_image_name = ""
 
             try:
                 shoper_unic = i.get("unic_name")
@@ -115,15 +85,8 @@ def copy_all_product_images_from_shoper_api():
                     (str(image.shoper_gfx_id) != str(shoper_gfx_id))
                     or (str(image.shoper_product_id) != str(shoper_product_id))
                     or (str(image.shoper_main) != str(shoper_main))
-                    or (str(image.shoper_title_pl) != str(shoper_title_pl))
-                    or (str(image.shoper_title_en) != str(shoper_title_en))
-                    or (str(image.shoper_title_de) != str(shoper_title_de))
-                    or (str(image.shoper_title_fr) != str(shoper_title_fr))
                     or (str(image.shoper_order) != str(shoper_order))
-                    or (str(image.shoper_link_pl) != str(shoper_link_pl))
-                    or (str(image.shoper_link_en) != str(shoper_link_en))
-                    or (str(image.shoper_link_de) != str(shoper_link_de))
-                    or (str(image.shoper_link_fr) != str(shoper_link_fr))
+                    or (str(image.shoper_image_name) != str(shoper_image_name))
                     or (str(image.shoper_unic) != str(shoper_unic))
                     or (str(image.shoper_hidden) != str(shoper_hidden))
                     or (str(image.shoper_extension) != str(shoper_extension))
@@ -131,19 +94,10 @@ def copy_all_product_images_from_shoper_api():
                     image.shoper_gfx_id = shoper_gfx_id
                     image.shoper_product_id = shoper_product_id
                     image.shoper_main = shoper_main
-                    image.shoper_title_pl = shoper_title_pl
-                    image.shoper_title_en = shoper_title_en
-                    image.shoper_title_de = shoper_title_de
-                    image.shoper_title_fr = shoper_title_fr
                     image.shoper_order = shoper_order
-
-                    image.shoper_link_pl = shoper_link_pl
-                    image.shoper_link_en = shoper_link_en
-                    image.shoper_link_de = shoper_link_de
-                    image.shoper_link_fr = shoper_link_fr
+                    image.shoper_image_name = shoper_image_name
                     image.shoper_unic = shoper_unic
                     image.shoper_hidden = shoper_hidden
-
                     image.shoper_extension = shoper_extension
                     image.save()
                     print(f"UPDATED: {image}")
@@ -154,15 +108,8 @@ def copy_all_product_images_from_shoper_api():
                     shoper_gfx_id=shoper_gfx_id,
                     shoper_product_id=shoper_product_id,
                     shoper_main=shoper_main,
-                    shoper_title_pl=shoper_title_pl,
-                    shoper_title_en=shoper_title_en,
-                    shoper_title_de=shoper_title_de,
-                    shoper_title_fr=shoper_title_fr,
                     shoper_order=shoper_order,
-                    shoper_link_pl=shoper_link_pl,
-                    shoper_link_en=shoper_link_en,
-                    shoper_link_de=shoper_link_de,
-                    shoper_link_fr=shoper_link_fr,
+                    shoper_image_name=shoper_image_name,
                     shoper_unic=shoper_unic,
                     shoper_hidden=shoper_hidden,
                     shoper_extension=shoper_extension,

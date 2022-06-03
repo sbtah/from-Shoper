f5042a95a80a0b9eae3d2550fdcef17c16d56598
sbtah <shisastore@protonmail.com>
2022-06-03 21:18:11 +0100

    Modified Image object to reflect new design of translation objects on Image

--- images/models.py
+++ images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from translations.models import ImageTranslation
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -6,13 +7,9 @@ class Image(models.Model):
     """Class for Image object."""
 
     # Shoper Data
-    shoper_gfx_id = models.IntegerField(unique=True, blank=True, null=True)
+    shoper_gfx_id = models.IntegerField(unique=True)
     shoper_product_id = models.IntegerField(blank=True, null=True)
     shoper_main = models.IntegerField(blank=True, null=True)
-    shoper_title_pl = models.CharField(max_length=100, blank=True, null=True)
-    shoper_title_en = models.CharField(max_length=100, blank=True, null=True)
-    shoper_title_de = models.CharField(max_length=100, blank=True, null=True)
-    shoper_title_fr = models.CharField(max_length=100, blank=True, null=True)
 
     class Order(models.IntegerChoices):
         first = 1
@@ -37,26 +34,12 @@ class Image(models.Model):
         blank=True,
         null=True,
     )
-    shoper_link_pl = models.CharField(
-        max_length=255, unique=True, blank=True, null=True
-    )
-    shoper_link_en = models.CharField(
-        max_length=255, unique=True, blank=True, null=True
-    )
-    shoper_link_de = models.CharField(
-        max_length=255, unique=True, blank=True, null=True
-    )
-    shoper_link_fr = models.CharField(
-        max_length=255, unique=True, blank=True, null=True
-    )
+    shoper_image_name = models.CharField(max_length=255, blank=True)
     shoper_unic = models.CharField(max_length=255)
     shoper_hidden = models.CharField(max_length=1, blank=True, null=True)
     shoper_extension = models.CharField(max_length=5, blank=True, null=True)
-    # Shopify Data
-    shopify_id = models.IntegerField(unique=True, blank=True, null=True)
-    shopify_product_id = models.CharField(max_length=20, blank=True, null=True)
-    shopify_link = models.CharField(max_length=255, unique=True, blank=True, null=True)
-    # Local Data
+    translations = models.ManyToManyField(ImageTranslation, blank=True)
+    # shoper_csv_image_link = xxx
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -66,4 +49,4 @@ class Image(models.Model):
 
     def __str__(self):
 
-        return f"ImageID:{self.shoper_gfx_id} | {self.shoper_title_pl}"
+        return f"ImageID:{self.shoper_gfx_id} | ProductID:{self.shoper_product_id}"
